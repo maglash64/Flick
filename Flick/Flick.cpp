4711fb45ca3b8b85c2862d0bee6b8433eae7a3f6
Titan <Titan@LAPTOP-OR9DJKMB>
2016-12-19 14:03:52 +0530

    Add project files.

---
+++ Flick/Flick.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hin, HINSTANCE hPin, LPWSTR lpcmdline, int nshow)
+{
+	HWND hwnd;
+	while (1)
+	{
+		hwnd = GetForegroundWindow();
+
+	}
+	return 0;
+}
